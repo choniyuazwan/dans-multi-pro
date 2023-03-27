fefac6a8a5b05a3376ad48d88e528f7e206a7807
choniyuazwan <choniyuazwan@gmail.com>
2023-03-27 18:08:26 +0700

    add logic for separate position based on location

--- src/main/java/id/choniyuazwan/onion/manager/PositionManager.java
+++ src/main/java/id/choniyuazwan/onion/manager/PositionManager.java
@@ -7,7 +7,10 @@ import org.slf4j.LoggerFactory;
 
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PositionManager {
   private static final Logger log = LoggerFactory.getLogger(PositionManager.class);
@@ -19,7 +22,26 @@ public class PositionManager {
   }
   
   public List<PositionDto> getListPosition() throws NoSuchAlgorithmException, KeyManagementException {
-    return positionService.getListPosition();
+    List<PositionDto> positionList = positionService.getListPosition();
+    
+    Map<String, List<PositionDto>> locationMap = new HashMap<>();
+    for (PositionDto data : positionList) {
+      String location = data.getLocation();
+      if (!locationMap.containsKey(location)) {
+        locationMap.put(location, new ArrayList<>());
+      }
+      locationMap.get(location).add(data);
+    }
+    
+    for (String location : locationMap.keySet()) {
+      System.out.println("Location: " + location);
+      List<PositionDto> locationDataList = locationMap.get(location);
+      for (PositionDto data : locationDataList) {
+        System.out.println("\t" + data.getId() + " - " + data.getType());
+      }
+    }
+    
+    return positionList;
   }
 
   public PositionDto getOnePosition(String id) {
